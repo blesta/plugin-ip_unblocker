3f970cd93f201ad62992a4139e861335d71975c0
Jonathan Reissmueller <jonathan@phillipsdata.com>
2020-05-01 14:54:44 -0700

    Initial commit

---
+++ language/en_us/ip_unblocker_plugin.php
@@ -0,0 +1,6 @@
+<?php
+$lang['IpUnblockerPlugin.!success.unblock_ip'] = 'Unblock request sent.';
+
+$lang['IpUnblockerPlugin.!error.remote_curl'] = 'Could not connect to the remote server. cURL Error: %1$s'; // %1$s is the curl error
+
+$lang['IpUnblockerPlugin.unblock_ip'] = 'Unblock IP';
